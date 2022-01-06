d58c7071db66df6384ba5de085f3d597cdec38b1
tubi55 <tubi55@nate.com>
2022-01-06 19:32:28 +0900

    시간에 따른 반응형 구현

--- js/main.js
+++ js/main.js
@@ -1,16 +1,58 @@
-﻿let numbers = document.querySelectorAll(".screen span");
+﻿const main = document.querySelector("main");
+let numbers = document.querySelectorAll(".screen span");
+let btns = document.querySelectorAll("nav span");
 const [am, pm] = document.querySelectorAll(".screen em");
+let hr = null;
+
 
 setInterval(()=>{		
-	const times = setTime();
+	let now = new Date();
+	hr = now.getHours();	
+
+	const data = [
+		{condition: hr>=5 && hr<11, name: "moninig"},
+		{condition: hr>=11 && hr<16, name: "afternoon"},
+		{condition: hr>=16 && hr<19, name: "evening"},
+		{condition: hr>=19, name: "night"}
+	]
+
+	data.forEach((item,index)=>{
+		if(item.condition){
+			main.className='';
+			main.classList.add(item.name);
+			for(let btn of btns) btn.classList.remove("on");
+			btns[index].classList.add("on");
+		}
+	})
+	/*
+	if(hr>=5 && hr<11 ){
+		main.className='';
+		main.classList.add("morning");
+	} 
+	if(hr>=11 && hr<16){
+		main.className='';
+		main.classList.add("afternoon");
+	} 
+	if(hr>=16 && hr<19){
+		main.className='';
+		main.classList.add("evening");
+	} 
+	if(hr>=19){
+		main.className='';
+		main.classList.add("night");
+	}
+	*/
+	
+
+	const times = setTime(now);
 
 	times.forEach((item, index)=> getTime(item, index));	
 },1000);
 
 
-function setTime(){
+
+function setTime(now){
 	let hr2 = null;
-	let now = new Date();
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
@@ -24,6 +66,7 @@ function setTime(){
 		am.classList.add('on');
 		pm.classList.remove('on');
 	}  
+
 	return [hr2, min, sec];
 }
 
@@ -31,19 +74,3 @@ function getTime(num,index){
 	if(num<10) num = "0"+num;
 	numbers[index].innerText = num;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
