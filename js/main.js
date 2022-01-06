6beba25b4dc5de20cc67c40a4b3c8066895ae9b7
tubi55 <tubi55@nate.com>
2022-01-06 18:20:11 +0900

    시간 수정 함수 개선

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-﻿let timer;
+﻿let numbers = document.querySelectorAll(".screen span");
 
-setInterval(()=>{
-	let time = document.querySelectorAll(".screen span");
+//1초마다 반복실행
+setInterval(()=>{	
+	//setTimes를 호출에 tiems변수에 배열 리턴 (hr, min, sec)
+	const times = setTime();
+
+	//배열안의 hr, min, sec을 반복 돌면서 
+	//getTime함수 호출하여 숫자가 한자리이면 앞에 0을 추가
+	times.forEach((item, index)=> getTime(item, index));	
+},1000);
+
+//시간 값을로부터 각각 시간, 분, 초를 구해서 배열로 리턴
+function setTime(){
 	let now = new Date();
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
+	return [hr, min, sec];
+}
 
-	if(hr < 10) hr = "0"+hr;
-	if(min < 10) min = "0"+min;
-	if(sec < 10) sec = "0"+sec;
-
-	time[0].innerText = hr;
-	time[1].innerText = min;
-	time[2].innerText = sec;
-},1000);
+//첫번째 인수로 수정할 숫자를 받고
+//두번째 인수로 변경할 numbers항목의 순서를 전달받음
+function getTime(num,index){
+	if(num<10) num = "0"+num;
+	numbers[index].innerText = num;
+}
 
 
 
