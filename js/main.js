5061c9cbb2b7bbcd233500c47ff4466eba2a25ec
tubi55 <tubi55@nate.com>
2022-01-06 18:08:21 +0900

    기본 시간 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-﻿
+﻿let timer;
+
+setInterval(()=>{
+	let time = document.querySelectorAll(".screen span");
+	let now = new Date();
+	let hr = now.getHours();
+	let min = now.getMinutes();
+	let sec = now.getSeconds();
+
+	if(hr < 10) hr = "0"+hr;
+	if(min < 10) min = "0"+min;
+	if(sec < 10) sec = "0"+sec;
+
+	time[0].innerText = hr;
+	time[1].innerText = min;
+	time[2].innerText = sec;
+},1000);
+
 
 
 
